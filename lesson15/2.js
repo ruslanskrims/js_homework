93b57133747ed134e47e292cae8e957abd04d58f
Ruslan <krims.ruslan@gmail.com>
2020-07-03 16:05:52 +0300

    Added Homework N15

---
+++ lesson15/2.js
@@ -0,0 +1,41 @@
+/**
+ * Задача 2.
+ *
+ * Дан класс Developer.
+ *
+ * У класса два свойства:
+ * - completedTasks — массив с выполненными задачами;
+ * - completeTask — метод для пополнения массива с выполненными задачами новой выполненной задачей.
+ *
+ * При создании двух экземпляров класса Developer,
+ * если выполнил задачу – почему-то эта-же задача
+ * считается выполненной и вторым вторым разработчиком.
+ *
+ * Исправьте этот пример так, чтобы при выполнении задачи одним разработчиком
+ * массив completedTasks второго разработчика никак не менялся.
+ * Нужно, чтобы у каждого разработчика был свой независимый массив completedTasks.
+ *
+ *  Условия:
+ * - Решить задачу в стиле прототипного наследования JavaScript.
+ */
+
+function Developer() {
+  this.completedTasks = [];
+}
+
+Developer.prototype.completedTasks = [];
+Developer.prototype.completeTask = function (task) {
+  this.completedTasks.push(task);
+};
+
+const developer1 = new Developer();
+const developer2 = new Developer();
+
+developer1.completeTask("finish a feature");
+developer1.completeTask("refactor code");
+developer2.completeTask("fix bug");
+
+console.log(developer1.completedTasks.length); // 2
+console.log(developer2.completedTasks.length); // 1
+
+exports.Developer = Developer;
